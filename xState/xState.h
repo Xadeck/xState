fcc25097ed5048be942d1728356ccf831303af1b
Xavier Décoret <xdecoret@xdecoret-macbookair.roam.corp.google.com>
2016-11-15 07:40:52 +0100

    Initial Xcode project

---
+++ xState/xState.h
@@ -0,0 +1,19 @@
+//
+//  xState.h
+//  xState
+//
+//  Created by Xavier Francois Decoret on 11/15/16.
+//  Copyright © 2016 Xavier Decoret. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for xState.
+FOUNDATION_EXPORT double xStateVersionNumber;
+
+//! Project version string for xState.
+FOUNDATION_EXPORT const unsigned char xStateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xState/PublicHeader.h>
+
+
